4d35c8a2f337b544140918f38371651e36513cac
Shrinidhi A <shrinidhisringeri@gmail.com>
2025-01-09 11:37:36 +0530

    init

---
+++ Examples/Basic-Books-FullStack-Demo/frontEnd/book-list-app/src/app/book.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Book } from './book';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookService {
+  private apiUrl = 'https://localhost:7095';
+
+  constructor(private http: HttpClient) {}
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.apiUrl);
+  }
+}
