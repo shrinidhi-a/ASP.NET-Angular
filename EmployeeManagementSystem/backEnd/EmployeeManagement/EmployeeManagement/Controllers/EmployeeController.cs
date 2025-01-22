a9ab2655fdb9938e063c377bdfd7329f81dc4069
Shrinidhi A <shrinidhisringeri@gmail.com>
2025-01-22 14:06:27 +0530

    Employee Management .Net Backend

---
+++ EmployeeManagementSystem/backEnd/EmployeeManagement/EmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,62 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetAllEmployeeAsync()
+        {
+            var allEmployee = await _employeeRepository.GetAllAsync();
+            return Ok(allEmployee);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployeeById(int id)
+        {
+            var employee = await _employeeRepository.GetByIdAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+        {
+            await _employeeRepository.AddEmployeeAsync(employee);
+            return CreatedAtAction(nameof(GetEmployeeById), new {id = employee.Id}, employee); //Even if ID is 0, it sets the generated ID.
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteEmployeeById(int id)
+        {
+            await _employeeRepository.DeleteEmployeeAsync(id);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Employee>> UpdateEmployeeById(int id, Employee employee)
+        {
+            if(id != employee.Id)
+            {
+                return BadRequest();
+            }
+
+            await _employeeRepository.UpdateEmployeeAsync(employee);
+            return CreatedAtAction(nameof(GetEmployeeById), new { id = employee.Id }, employee);
+        }
+    }
+}
